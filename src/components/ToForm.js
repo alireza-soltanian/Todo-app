a4ef420ba4b4f17e442b5b0fa867fe625f3345f3
alireza-soltanian <141966029+alireza-soltanian@users.noreply.github.com>
2023-12-02 06:25:14 +0330

    ToDo-app react
    Saturday, 2 December 2023

---
+++ src/components/ToForm.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+export const ToForm = ({ addTodo }) => {
+  const [value, setvalue] = useState("")
+
+  const handlesubmit = e => {
+    e.preventDefault();
+
+    addTodo(value)
+
+    setvalue("")
+  }
+  return (
+    <form className='TodoForm' onSubmit={handlesubmit}>
+      <input type="text" className='todo-input' value={value}
+        placeholder='what is the task today?' onChange={(e) => setvalue(e.target.value)} />
+      <button type='submit' className='todo-btn'>add task</button>
+    </form>
+  )
+}
